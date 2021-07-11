cd1d2f6005c20bec3e1b8857b072ee8ff61bd93c
Nicholas McGinty <nmcginty97@gmail.com>
2021-07-11 17:47:48 -0400

    Resolved rsync issue where it was pushing entire drexel directory to tux and not single class

--- rsync/drexel-courses.sh
+++ rsync/drexel-courses.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
-rsync -avzP --exclude={'.venv','__pycache__'} ~/code/school/drexel/${CURRENT_DREXEL_COURSE} nm3249@tux:~/courses/
+rsync -avzP --delete --exclude={'.venv','__pycache__'} ~/code/school/drexel/${CURRENT_DREXEL_COURSE}/ nm3249@tux:~/courses/${CURRENT_DREXEL_COURSE}
 #rsync -avzP --delete ~/code/school/drexel/${course} nm3249@tux:~/courses/
