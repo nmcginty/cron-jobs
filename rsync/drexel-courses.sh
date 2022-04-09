c6c4bd3bfa6692c7e55f91d6a59795757a4398a8
Nicholas R McGinty <nmcginty97@gmail.com>
2022-04-09 02:03:39 -0400

    Update long failing rsync command

--- rsync/drexel-courses.sh
+++ rsync/drexel-courses.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/zsh
 
-rsync -avzP --delete --exclude={'.venv','__pycache__','.git','*.csv','*.pkl','*.data'} \
-      ~/code/school/drexel/${CURRENT_DREXEL_COURSE}/ nm3249@tux:~/courses/${CURRENT_DREXEL_COURSE}
+rsync -avzP \
+      --delete 
+      --exclude={'.venv','__pycache__','.git','*.csv','*.pkl','*.data'} \
+      ~/code/school/drexel/${CURRENT_DREXEL_COURSE} \
+      nm3249@tux:~/courses
